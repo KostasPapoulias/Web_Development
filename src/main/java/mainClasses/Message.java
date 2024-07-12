f0233608c1ad0e581797a48af590e9150eee5322
Konstantinos Papoulias <kostaspapoulias@gmail.com>
2024-07-12 12:12:40 +0300

    new main classes and classes editing the database

---
+++ src/main/java/mainClasses/Message.java
@@ -0,0 +1,53 @@
+package mainClasses;
+
+/**
+ *
+ * @author mountant
+ */
+public class Message {
+    int message_id, booking_id;
+    String message,datetime,sender;
+
+    public int getBooking_id() {
+        return booking_id;
+    }
+
+    public void setBooking_id(int booking_id) {
+        this.booking_id = booking_id;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public int getMessage_id() {
+        return message_id;
+    }
+
+    public void setMessage_id(int message_id) {
+        this.message_id = message_id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getDatetime() {
+        return datetime;
+    }
+
+    public void setDatetime(String datetime) {
+        this.datetime = datetime;
+    }
+
+
+
+}
